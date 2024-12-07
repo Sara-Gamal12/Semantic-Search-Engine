3c78600a7625ab2dee6469001cbaeb7371957f49
Eman <ibrahim.eman83@gmail.com>
2024-12-07 13:01:07 +0200

    change read centroids

--- utils.py
+++ utils.py
@@ -16,20 +16,31 @@ def write_file_centroids(file_path,centroids):
             
        
 def read_file_centroids_with_memap(file_path):
+            
         
             # Memory-map the file
-            mmap_data = np.memmap(file_path, dtype=np.uint8, mode='r')
+            mmap_data = np.memmap(file_path, dtype=np.float32, mode='r')
     
-
-            # Precompute the struct formats
-           
-
             # Calculate the number of centroids
-            num_centroids = len(mmap_data) // (DIMENSION * ELEMENT_SIZE)
+            num_centroids = len(mmap_data) // DIMENSION
             
             # Reshape the mmap_data to a 2D array of centroids
-            centroids = np.frombuffer(mmap_data, dtype=np.float32, count=num_centroids * DIMENSION).reshape(num_centroids, DIMENSION)
+            centroids = mmap_data.reshape(num_centroids, DIMENSION)
             return centroids
+        
+            # # Memory-map the file
+            # mmap_data = np.memmap(file_path, dtype=np.uint8, mode='r')
+    
+
+            # # Precompute the struct formats
+           
+
+            # # Calculate the number of centroids
+            # num_centroids = len(mmap_data) // (DIMENSION * ELEMENT_SIZE)
+            
+            # # Reshape the mmap_data to a 2D array of centroids
+            # centroids = np.frombuffer(mmap_data, dtype=np.float32, count=num_centroids * DIMENSION).reshape(num_centroids, DIMENSION)
+            # return centroids
     
 
 def write_file_records(file_path, data):
